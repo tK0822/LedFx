11f4abd08bc2b8fe3279cf9e0707756a42058921
tK0822 <tobias.kerbl@tum.de>
2023-03-23 20:27:33 +0100

    "fade": smoother fading

--- ledfx/effects/fade.py
+++ ledfx/effects/fade.py
@@ -23,6 +23,24 @@ class FadeEffect(TemporalEffect, GradientEffect):
         }
     )
 
+    _gradient_samples = 200
+
+    def _assert_gradient(self):
+        if (
+            self._gradient_curve is None  # Uninitialized gradient
+            or len(self._gradient_curve[0])
+            !=self._gradient_samples  # Incorrect size
+        ):
+            self._generate_gradient_curve(
+                self._config["gradient"],
+                self._gradient_samples,
+            )
+    
+    def get_gradient_color(self, point):
+        self._assert_gradient()
+
+        return self._gradient_curve[:, int((self._gradient_samples - 1) * point)]
+
     def config_updated(self, config):
         self.idx = 0
         self.forward = True
@@ -31,11 +49,11 @@ class FadeEffect(TemporalEffect, GradientEffect):
         pass
 
     def effect_loop(self):
-        self.idx += 0.0015
+        self.idx += 0.015
         if self.idx > 1:
             self.idx = 1
             self.forward = not self.forward
-        self.idx = self.idx % 1
+            self.idx = self.idx % 1
 
         if self.forward:
             i = self.idx
