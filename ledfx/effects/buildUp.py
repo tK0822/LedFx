f8eca5c97cab6d732fdb00db1be23f9935321c2d
tK0822 <tobias.kerbl@tum.de>
2023-03-23 20:27:33 +0100

    "Build Up": Added first working version

--- ledfx/effects/buildUp.py
+++ ledfx/effects/buildUp.py
@@ -5,13 +5,14 @@ from ledfx.color import parse_color, validate_color, LEDFX_COLORS
 from ledfx.effects.temporal import TemporalEffect
 
 
-class RunningDotEffect(TemporalEffect):
-    NAME = "Running Dot"
+class BuildUp(TemporalEffect):
+    NAME = "Build Up"
     CATEGORY = "Non-Reactive"
 
-    _dot_color = None
+    _color = None
     _idx = 0
     _forward = True
+    _black = np.array(parse_color(LEDFX_COLORS["black"]), dtype=float)
 
     CONFIG_SCHEMA = vol.Schema(
         {
@@ -22,25 +23,30 @@ class RunningDotEffect(TemporalEffect):
     )
 
     def config_updated(self, config):
-        self._dot_color = np.array(parse_color(self._config["color"]), dtype=float)
+        self._color = np.array(parse_color(self._config["color"]), dtype=float)
         self._idx = 0
 
     def on_activate(self, pixel_count):
         pass
 
     def effect_loop(self):
+        if self._forward:
+            self.pixels[self._idx] = self._color
+        else:
+            self.pixels[self._idx] = self._black
         
-        color_array = np.tile(np.array(parse_color(LEDFX_COLORS["black"]), dtype=float), (self.pixel_count, 1))
-        color_array[self._idx] = self._dot_color
-        self.pixels = color_array
-
         if self._idx < self.pixel_count - 1 and self._forward:
             self._idx += 1
         elif self._idx > 0 and not self._forward: 
             self._idx -= 1
-         
-        if self._idx == self.pixel_count - 1 or self._idx == 0:
+
+        if self._idx == self.pixel_count - 1  or self._idx == 0:
+            self.pixels[self._idx] = self._color
             self._forward = not self._forward
+
+        
+
+        
         
 
             
